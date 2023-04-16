596ca166bbe5d94e5114d5d9cd1c9cddc2828982
Muhtasim Mushfiq <muhtasim01@gmail.com>
2023-04-16 01:06:27 -0400

    Added all brothers' names

--- theta_tau/pages/brothers.jsx
+++ theta_tau/pages/brothers.jsx
@@ -19,10 +19,37 @@ const Brothers = () => {
           <Brother style={styles.brother} name="Manvir Singh" title="Pledge Marshal" image="../assets/elise.png"/>
 
           <Text style={styles.title}>ACTIVE BROTHERS</Text>
-          <Brother style={styles.brother} name="Elise Dougherty" title="Regent" image="../assets/elise.png"/>
-          <Brother style={styles.brother} name="Elise Dougherty" title="Regent" image="../assets/elise.png"/>
-          <Brother style={styles.brother} name="Elise Dougherty" title="Regent" image="../assets/elise.png"/>
-          <Brother style={styles.brother} name="Elise Dougherty" title="Regent" image="../assets/elise.png"/>
+          <Brother style={styles.brother} name="David Huang" title="" image="../assets/elise.png"/>
+          <Brother style={styles.brother} name="Felix Bermudez" title="" image="../assets/elise.png"/>
+          <Brother style={styles.brother} name="David Zaretsky" title="" image="../assets/elise.png"/>
+          <Brother style={styles.brother} name="Billy Grippi" title="" image="../assets/elise.png"/>
+          <Brother style={styles.brother} name="Max Gustin" title="" image="../assets/elise.png"/>
+          <Brother style={styles.brother} name="Del Hart" title="" image="../assets/elise.png"/>
+          <Brother style={styles.brother} name="Matthew Amato" title="" image="../assets/elise.png"/>
+          <Brother style={styles.brother} name="Dennis Cetaj" title="" image="../assets/elise.png"/>
+          <Brother style={styles.brother} name="Kelly Chan" title="" image="../assets/elise.png"/>
+          <Brother style={styles.brother} name="Edward Masalimov" title="" image="../assets/elise.png"/>
+          <Brother style={styles.brother} name="Tenzin Pema" title="" image="../assets/elise.png"/>
+          <Brother style={styles.brother} name="Chloe Tullius" title="" image="../assets/elise.png"/>
+          <Brother style={styles.brother} name="Gabriel Thornton" title="" image="../assets/elise.png"/>
+          <Brother style={styles.brother} name="James Fattorusso" title="" image="../assets/elise.png"/>
+          <Brother style={styles.brother} name="Jeffrey Kou" title="" image="../assets/elise.png"/>
+          <Brother style={styles.brother} name="Andrew Li" title="" image="../assets/elise.png"/>
+          <Brother style={styles.brother} name="Victor Lin" title="" image="../assets/elise.png"/>
+          <Brother style={styles.brother} name="Myles Pauquette" title="" image="../assets/elise.png"/>
+          <Brother style={styles.brother} name="Juan Quintero" title="" image="../assets/elise.png"/>
+          <Brother style={styles.brother} name="Adrian Sin" title="" image="../assets/elise.png"/>
+          <Brother style={styles.brother} name="Justin Siwa" title="" image="../assets/elise.png"/>
+          <Brother style={styles.brother} name="Alex Yevzerov" title="" image="../assets/elise.png"/>
+          <Brother style={styles.brother} name="Zachary Clark" title="" image="../assets/elise.png"/>
+          <Brother style={styles.brother} name="James Henry" title="" image="../assets/elise.png"/>
+          <Brother style={styles.brother} name="Anna Kane" title="" image="../assets/elise.png"/>
+          <Brother style={styles.brother} name="Isabella Nguyen" title="" image="../assets/elise.png"/>
+          <Brother style={styles.brother} name="Rrucha Singh" title="" image="../assets/elise.png"/>
+          <Brother style={styles.brother} name="Hao Zhu" title="" image="../assets/elise.png"/>
+          <Brother style={styles.brother} name="Jenny Zhu" title="" image="../assets/elise.png"/>
+
+
           
           <Extra />
         </ScrollView>
