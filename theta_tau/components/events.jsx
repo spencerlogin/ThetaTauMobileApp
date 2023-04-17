c6239af54ba5ea3f52d5176a96e0b288bed7b00c
Ryan Khan <ryankhan@buffalo.edu>
2023-04-17 18:45:21 -0400

    rush events

--- theta_tau/components/events.jsx
+++ theta_tau/components/events.jsx
@@ -1,37 +1,57 @@
 import React from 'react';
-import { View, Text, Image, TouchableOpacity, StyleSheet, Dimensions, Pressable } from 'react-native';
+import { View, Text, Image, StyleSheet, ScrollView, Pressable } from 'react-native';
 import { useNavigation } from '@react-navigation/native';
 
 const Brother = (props) => {
   const navigation = useNavigation();
 
+  // Placeholder image sources
+  const placeholderImages = [
+    require('./IMG_7473.jpg'),
+    require('./IMG_7472.jpg'),
+    require('./IMG_7471.jpg'),
+    require('./IMG_7470.jpg'),
+    require('./IMG_7469.jpg'),
+    require('./IMG_7467.jpg'),
+    require('./IMG_7468.jpg'),
+  ];
+
   return (
-    <Pressable style={styles.container} onPress={() => {navigation.navigate('BrotherPage')}}>
-      <View style={styles.column}>
-        <Text style={styles.text}>Hello</Text>
-      </View>
-    </Pressable>
+    <View style={styles.container}>
+      <ScrollView horizontal showsHorizontalScrollIndicator={false}>
+        {/* Render placeholder images */}
+        {placeholderImages.map((source, index) => (
+          <Image key={index} source={source} style={styles.image} />
+        ))}
+      </ScrollView>
+      <Pressable onPress={() => navigation.navigate('BrotherPage')} style={styles.button}>
+        <Text style={styles.buttonText}>RUSH EVENTS</Text>
+      </Pressable>
+    </View>
   );
 };
 
 const styles = StyleSheet.create({
-    container: {
-      backgroundColor: '#710000',
-      flexDirection: 'row', 
-      justifyContent: 'space-between', 
-      alignItems: 'center', 
-      padding: 10,
-      marginBottom: 10,
-    },
-    text: {
-        color: '#E29900',
-    },
-    column: {
-      justifyContent: 'center', 
-      alignItems: 'center',
-      flex: 1, 
-      marginHorizontal: 5,
-    }
-  });
+  container: {
+    backgroundColor: '#710000',
+    padding: 10,
+    marginBottom: 10,
+  },
+  image: {
+    width: 100,
+    height: 100,
+    marginHorizontal: 5,
+  },
+  button: {
+    backgroundColor: '#E29900',
+    justifyContent: 'center',
+    alignItems: 'center',
+    padding: 10,
+    marginTop: 10,
+  },
+  buttonText: {
+    color: '#FFF',
+  },
+});
 
 export default Brother;
