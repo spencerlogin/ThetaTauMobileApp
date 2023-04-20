c46eaa06abe1275fb73211ae45430c8de17fd1df
Muhtasim Mushfiq <muhtasim01@gmail.com>
2023-04-19 22:51:03 -0400

    added me on easter egg

--- theta_tau/pages/easterEgg.jsx
+++ theta_tau/pages/easterEgg.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { View, Text, Image, TouchableOpacity, StyleSheet, Dimensions, Pressable } from 'react-native';
+import { View, Text, Image, TouchableOpacity, ScrollView, StyleSheet, Dimensions, Pressable } from 'react-native';
 import { useNavigation } from '@react-navigation/native';
 import { SafeAreaView } from 'react-native-safe-area-context';
 
@@ -7,10 +7,14 @@ const Extra = () => {
   //Test
   return (
     <SafeAreaView>
+    <ScrollView>
         <Text style={{fontSize: 50}}>Mathew Yeung was here</Text>        
-        <Text style={{fontSize: 10}}>and Muhtasim</Text>
         <Image style={{width: 400, height: 400}} source={require('../assets/mat.png')} />
+        <Text style={{fontSize: 50}}>Muhtasim GOAT PM</Text>
+        <Image style={{width: 400, height: 400}} source={require('../assets/muhtasim.png')} />
+      </ScrollView>
     </SafeAreaView>
+    
   );
 };
 
